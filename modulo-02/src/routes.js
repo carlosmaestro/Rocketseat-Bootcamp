18349fe8b3a21c46604ad0205a3015f4704ed511
Carlos Maestro <carlos.maestro@memora.com.br>
2019-08-15 17:56:23 -0300

    modulo02 nodemo, sucrase e estrutura basica

--- modulo-02/src/routes.js
+++ modulo-02/src/routes.js
@@ -1,1 +1,11 @@
-const 
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.get('/', (req, res)=>{
+  return res.send({
+    message: 'Hello World'
+  });
+});
+
+export default routes;
