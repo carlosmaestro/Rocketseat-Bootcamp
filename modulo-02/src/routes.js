2c58ce87de8d635fb96e4bf1c5990700d366950e
Carlos Maestro <carlos.maestro@memora.com.br>
2019-08-16 17:52:21 -0300

    modulo02 configurando modelo usuario

--- modulo-02/src/routes.js
+++ modulo-02/src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.send({
-    message: 'Hello World',
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Carlos',
+    email: 'chmmaestro@gmail.com',
+    password_hash: '1234567789',
   });
+  return res.json(user);
 });
 
 export default routes;
