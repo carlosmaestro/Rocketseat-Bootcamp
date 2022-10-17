e4b5897e964d19e1ace219d5e52da2b2f02b2d1a
Carlos Maestro <chmmaestro@gmail.com>
2022-10-17 17:41:56 -0300

    adicionando sidebar

--- ignite-2022/01-fundamentos-reactjs/src/App.jsx
+++ ignite-2022/01-fundamentos-reactjs/src/App.jsx
@@ -3,16 +3,25 @@ import { Post } from "./Post";
 
 import './global.css';
 
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
+
 export function App() {
   return <>
     <Header />
-    <Post
-      author="Jose da Silva Sauro"
-      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit magnam corporis delectus. Molestiae eius, minus quidem amet repudiandae dolorum quaerat illum sint nesciunt corrupti ea recusandae omnis aliquam reiciendis iusto."
-    />
-    <Post
-      author="Luiza"
-      content="Post TOP!"
-    />
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+
+        <Post
+          author="Jose da Silva Sauro"
+          content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit magnam corporis delectus. Molestiae eius, minus quidem amet repudiandae dolorum quaerat illum sint nesciunt corrupti ea recusandae omnis aliquam reiciendis iusto."
+        />
+        <Post
+          author="Luiza"
+          content="Post TOP!"
+        />
+      </main>
+    </div>
   </>
 }
